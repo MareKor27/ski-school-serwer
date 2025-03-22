1ab8405d7edfca5b13d22553d4b38571835fcbbc
MareKor27 <marek.korzeniewski1996@gmail.com>
2025-03-22 22:28:47 +0100

    Replace hardcoded strings with environment variables

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,14 +7,18 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from 'src/commons/middleware/jwt-strategy';
 import { PasswordResetRequestModel } from './model/password-reset-request.model';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     SequelizeModule.forFeature([PasswordResetRequestModel]),
     UsersModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'SECRET_KEY',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' },
     }),
   ],
--- src/commons/middleware/jwt-strategy.ts
+++ src/commons/middleware/jwt-strategy.ts
@@ -2,17 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserData } from 'src/auth/type/auth';
-import { roles } from 'src/users/types/role';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Pobiera token z nagłówka
-      secretOrKey: 'SECRET_KEY', // Sekret do weryfikacji (zmienna środowiskowa w produkcji!)
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
-
   async validate(payload: UserData) {
     return { id: payload.id, email: payload.email, role: payload.role };
   }
